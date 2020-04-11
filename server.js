6232de415966a24e69ec3363fc5ecefd37299b88
Luc Berger <luc.richard1994@gmail.com>
2020-04-11 15:54:22 +0100

    Amend server.js file as the wrong folder was being referenced when running the build on a server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/dist'));
+	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
